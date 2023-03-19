6993168272b8ce14937db20337774b5894b7eff5
auutek <aulia.teaku@gmail.com>
2023-03-20 06:07:01 +0700

    main

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,19 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Route::resource('/', ViewController::class);
-// Route::get('politik', 'App\Http\Controllers\ViewController@index1');
-// Route::get('ekonomi', 'App\Http\Controllers\ViewController@index2');
-// Route::get('sosial', 'App\Http\Controllers\ViewController@index3');
-// Route::get('lingkungan', 'App\Http\Controllers\ViewController@index4');
-// Route::get('pendidikan', 'App\Http\Controllers\ViewController@index5');
-// Route::get('others', 'App\Http\Controllers\ViewController@index6');
-// Route::resource('post', PostController::class);
+Route::resource('/', ViewController::class);
+Route::get('politik', 'App\Http\Controllers\ViewController@index1');
+Route::get('ekonomi', 'App\Http\Controllers\ViewController@index2');
+Route::get('sosial', 'App\Http\Controllers\ViewController@index3');
+Route::get('lingkungan', 'App\Http\Controllers\ViewController@index4');
+Route::get('pendidikan', 'App\Http\Controllers\ViewController@index5');
+Route::get('others', 'App\Http\Controllers\ViewController@index6');
+Route::resource('post', PostController::class);
 
-// Route::get('/search', 'App\Http\Controllers\ViewController@search');
+Route::get('/search', 'App\Http\Controllers\ViewController@search');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
